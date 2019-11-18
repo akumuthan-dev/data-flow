f55be2010f42fc7b065652947461fac488eee123
Alexey Bezhan <al@fueledbylemons.com>
2019-11-18 15:03:35 +0000

    Move common functions to dataflow.utils
    Moves get_redis_client and adds a new get_nested_key helper to dataflow
    utils.
    
    Also defines a common FieldMapping type for the new field mapping format
    to be used in function type annotations.

--- dataflow/utils.py
+++ dataflow/utils.py
@@ -1,7 +1,33 @@
 """A module that defines useful utils."""
 
+from typing import Any, List, Tuple, Union
+
+import redis
+import sqlalchemy
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.postgres_operator import PostgresOperator
+
+from dataflow import config
+
+
+FieldMapping = List[Tuple[Union[str, Tuple], sqlalchemy.Column]]
+
+
+def get_redis_client():
+    """Returns redis client from connection URL"""
+    return redis.from_url(url=config.REDIS_URL, decode_responses=True)
+
+
+def get_nested_key(data: dict, path: Union[Tuple, str], required: bool = False) -> Any:
+    if isinstance(path, str):
+        path = (path,)
+
+    for key in path:
+        data = data[key] if required else data.get(key)
+        if not required and data is None:
+            return data
+
+    return data
 
 
 class XCOMIntegratedPostgresOperator(PostgresOperator):
