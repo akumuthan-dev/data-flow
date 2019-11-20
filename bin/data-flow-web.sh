0b9b7670d52cbd6b3842f988b1c687b104fc719b
Alexey Bezhan <al@fueledbylemons.com>
2019-11-20 16:26:30 +0000

    Set Airflow configuration from CF env variables on startup
    A new set of DB credentials is created on each PaaS deployment, so
    we need to set Airflow DB URL dynamically from env variables provided
    by CloudFoundry. We could either parse VCAP_SERVICES or use a helper
    DATABASE_URL env var.
    
    This is using POSIX parameter substitution supported by dash to replace
    postges:// prefix with a SQLAlchemy supported one.
    
    https://docs.cloudfoundry.org/devguide/deploy-apps/environment-variable.html#DATABASE-URL

--- bin/data-flow-web.sh
+++ bin/data-flow-web.sh
@@ -2,6 +2,9 @@
 
 export PYTHONPATH=/app:$PYTHONPATH
 
+export AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2${DATABASE_URL#postgres}"
+export AIRFLOW__CELERY__RESULT_BACKEND="db+postgresql${DATABASE_URL#postgres}"
+
 # Kill any running airflow processes
 cat $AIRFLOW_HOME/airflow-webserver.pid | xargs kill -9
 
--- bin/data-flow-worker.sh
+++ bin/data-flow-worker.sh
@@ -1,3 +1,6 @@
 export PYTHONPATH=/app
 
+export AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2${DATABASE_URL#postgres}"
+export AIRFLOW__CELERY__RESULT_BACKEND="db+postgresql${DATABASE_URL#postgres}"
+
 airflow worker
