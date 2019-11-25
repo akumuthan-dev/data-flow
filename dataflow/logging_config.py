e63fc6ae3f428b1e1e724e29eafb7888181983b4
Alexey Bezhan <al@fueledbylemons.com>
2019-11-25 16:26:54 +0000

    Redirect airflow scheduler logs to stdout
    Scheduler is writing a lot of logs (around 200Mb per day), potentially
    filling the allocated CloudFoundry storage if the app hasn't been
    restarted for a few weeks, which prevents scheduler from starting new
    tasks.
    
    Since version 1.9 Airflow started moving towards python logging
    configuration format, which allows overriding individual logger
    settings.
    
    This sets up a logging config based on the Airflow template which
    redirects taks logs to a file to be uploaded to S3 and everything
    else to stdout, which should be picked up by either docker-compose
    or CloudFoundry.
    
    https://github.com/apache/airflow/blob/master/airflow/config_templates/airflow_local_settings.py

---
+++ dataflow/logging_config.py
@@ -0,0 +1,59 @@
+import os
+
+from airflow.configuration import conf
+
+REMOTE_LOGGING = conf.getboolean("core", "remote_logging")
+
+BASE_LOG_FOLDER = conf.get("core", "BASE_LOG_FOLDER")
+REMOTE_BASE_LOG_FOLDER = conf.get("core", "REMOTE_BASE_LOG_FOLDER")
+
+FILENAME_TEMPLATE = "{{ ti.dag_id }}/{{ ti.task_id }}/{{ ts }}/{{ try_number }}.log"
+
+LOG_LEVEL = "INFO"
+DAG_PARSING_LOG_LEVEL = "WARN"
+
+LOG_FORMAT = (
+    "[%(asctime)s] %(levelname)s {%(name)s:%(filename)s:%(lineno)d} - %(message)s"
+)
+
+LOGGING_CONFIG: dict = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {"airflow.task": {"format": LOG_FORMAT}},
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "airflow.task",
+            "stream": "ext://sys.stdout",
+        },
+        "task": {
+            "class": "airflow.utils.log.s3_task_handler.S3TaskHandler",
+            "formatter": "airflow.task",
+            "base_log_folder": os.path.expanduser(BASE_LOG_FOLDER),
+            "s3_log_folder": REMOTE_BASE_LOG_FOLDER,
+            "filename_template": FILENAME_TEMPLATE,
+        }
+        if REMOTE_LOGGING
+        else {
+            "class": "airflow.utils.log.file_task_handler.FileTaskHandler",
+            "formatter": "airflow.task",
+            "base_log_folder": os.path.expanduser(BASE_LOG_FOLDER),
+            "filename_template": FILENAME_TEMPLATE,
+        },
+    },
+    "loggers": {
+        "airflow.task": {"handlers": ["task"], "level": LOG_LEVEL, "propagate": False},
+        "airflow.task_runner": {
+            "handlers": ["console"],
+            "level": LOG_LEVEL,
+            "propagate": True,
+        },
+        "airflow.processor": {
+            "handlers": ["console"],
+            "level": DAG_PARSING_LOG_LEVEL,
+            "propagate": False,
+        },
+        "airflow": {"handlers": ["console"], "level": LOG_LEVEL, "propagate": False},
+    },
+    "root": {"handlers": ["console"], "level": LOG_LEVEL},
+}
