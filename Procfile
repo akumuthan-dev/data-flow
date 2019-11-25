139de2a9b4efd10f453d3630202d9f82016dc6b1
Alexey Bezhan <al@fueledbylemons.com>
2019-11-25 12:13:43 +0000

    Set some security settings
    This overrides airflow default configuration in all PaaS environments:
    
    * Blocks any airflow API requests (was open to all by default) since
      we're not using the API at the moment
    * Enables "secure" mode, which removes ad-hoc queries, charts and some
      other functionality I don't think we use
    * Disables pickling of DAGs and XCOMs. XCOMs will be disabled by default
      in future airflow versions due to security concerns and since we're
      using the same version of code in airflow web node and workers we
      don't need to use pickling for DAG code.
    * Enable at rest encryption for S3 logs
    * Moves flower basic auth credentials to the env variable so they don't
      show up in `ps`
    * Sets secure and same-site flags on the airflow session cookies.
      Same-site set to Lax since Strict seem to break our current Oauth2
      setup.
    * Increases the interval with which scheduler scans for new DAGs. We're
      not modifying DAGs on running instances, so the scheduler starts with
      the current version of the code and will be replaced when a release
      happens, so there's no reason for it to check for DAG updates. There
      doesn't seem to be a way to disable it completely, so it's set to 1
      day for now.

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 web: /home/vcap/app/bin/paas-wrapper.sh airflow webserver -p 8080
 worker: /home/vcap/app/bin/paas-wrapper.sh airflow worker
 scheduler: /home/vcap/app/bin/paas-wrapper.sh airflow scheduler
-flower: /home/vcap/app/bin/paas-wrapper.sh airflow flower --basic_auth=$FLOWER_USERNAME:$FLOWER_PASSWORD
+flower: /home/vcap/app/bin/paas-wrapper.sh airflow flower
--- bin/paas-wrapper.sh
+++ bin/paas-wrapper.sh
@@ -4,23 +4,37 @@ export PYTHONPATH=/app:${PYTHONPATH}
 export AIRFLOW_HOME=/home/vcap/app/airflow
 export DEBUG=False
 
+export AIRFLOW__API__AUTH_BACKEND=airflow.api.auth.backend.deny_all
+
 export AIRFLOW__CORE__DAGS_FOLDER=/home/vcap/app/dataflow/dags
 export AIRFLOW__CORE__DAGS_ARE_PAUSED_AT_CREATION=True
 export AIRFLOW__CORE__LOAD_EXAMPLES=False
 
 export AIRFLOW_CONN_DEFAULT_S3="s3://"$(echo $VCAP_SERVICES | jq -r '.["aws-s3-bucket"][0].credentials | "\(.aws_access_key_id):\(.aws_secret_access_key)"')"@S3"
 
+export AIRFLOW__CORE__SECURE_MODE=True
+export AIRFLOW__CORE__DONOT_PICKLE=True
+export AIRFLOW__CORE__ENABLE_XCOM_PICKLING=False
+
 export AIRFLOW__CORE__REMOTE_LOGGING=True
 export AIRFLOW__CORE__REMOTE_LOG_CONN_ID=DEFAULT_S3
+export AIRFLOW__CORE__ENCRYPT_S3_LOGS=True
 export AIRFLOW__CORE__REMOTE_BASE_LOG_FOLDER="s3://"$(echo $VCAP_SERVICES | jq -r '.["aws-s3-bucket"][0].credentials.bucket_name')"/logs"
-
-export AIRFLOW__WEBSERVER__AUTHENTICATE=True
-export AIRFLOW__WEBSERVER__AUTH_BACKEND=dataflow.airflow_login
 
 export AIRFLOW__CORE__SQL_ALCHEMY_CONN=$(echo $VCAP_SERVICES | jq -r '.postgres[0].credentials.uri')
 
 export AIRFLOW__CORE__EXECUTOR=CeleryExecutor
 export AIRFLOW__CELERY__BROKER_URL=$(echo $VCAP_SERVICES | jq -r '.redis[0].credentials.uri')
 export AIRFLOW__CELERY__RESULT_BACKEND="db+${AIRFLOW__CORE__SQL_ALCHEMY_CONN}"
+export AIRFLOW__CELERY__FLOWER_BASIC_AUTH="${FLOWER_USERNAME}:${FLOWER_PASSWORD}"
+export AIRFLOW__CELERY__FLOWER_PORT=8080
+
+export AIRFLOW__SCHEDULER__DAG_DIR_LIST_INTERVAL=86400
+export AIRFLOW__SCHEDULER__PRINT_STATS_INTERVAL=86400
+
+export AIRFLOW__WEBSERVER__AUTHENTICATE=True
+export AIRFLOW__WEBSERVER__AUTH_BACKEND=dataflow.airflow_login
+export AIRFLOW__WEBSERVER__COOKIE_SECURE=True
+export AIRFLOW__WEBSERVER__COOKIE_SAMESITE=Lax
 
 exec $@
