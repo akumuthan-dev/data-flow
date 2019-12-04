1e7c61c11f15ef9205c956cf7a11eac78e81ba11
Nick Ross <nick.ir.ross@gmail.com>
2019-12-04 14:54:50 +0000

    Remove trailing whitespace

--- dataflow/dags/view_pipelines.py
+++ dataflow/dags/view_pipelines.py
@@ -670,15 +670,15 @@ class FDIMonthlyStaticViewPipeline(BaseViewPipeline):
     query = '''
         WITH fdi_report AS (
             WITH companies_last_version AS (
-                SELECT *  
+                SELECT *
                 FROM companies_dataset
                     LEFT JOIN (
-                        SELECT 
+                        SELECT
                             DISTINCT ON (company_id)
                             company_id AS joined_id,
                             contact_name AS contact_name,
                             is_primary,
-                            phone AS contact_phone, 
+                            phone AS contact_phone,
                             email AS contact_email,
                             accepts_dit_email_marketing AS contact_accepts_dit_email_marketing
                         FROM contacts_dataset
@@ -778,18 +778,18 @@ class FDIMonthlyStaticViewPipeline(BaseViewPipeline):
                 fdi.delivery_partners,
                 fdi.possible_uk_regions,
                 fdi.actual_uk_regions,
-                CASE 
-                  WHEN fdi.other_business_activity IS NULL 
-                       AND fdi.business_activities IS NOT NULL 
-                    THEN fdi.business_activities 
-                  WHEN fdi.other_business_activity IS NOT NULL 
-                       AND fdi.business_activities IS NULL 
+                CASE
+                  WHEN fdi.other_business_activity IS NULL
+                       AND fdi.business_activities IS NOT NULL
+                    THEN fdi.business_activities
+                  WHEN fdi.other_business_activity IS NOT NULL
+                       AND fdi.business_activities IS NULL
                     THEN fdi.other_business_activity
-                  WHEN fdi.other_business_activity IS NOT NULL 
-                       AND fdi.business_activities IS NOT NULL 
+                  WHEN fdi.other_business_activity IS NOT NULL
+                       AND fdi.business_activities IS NOT NULL
                     THEN fdi.other_business_activity || ', ' || fdi.business_activities
                   ELSE NULL END AS business_activities,
-                fdi.project_arrived_in_triage_on, 
+                fdi.project_arrived_in_triage_on,
                 fdi.proposal_deadline,
                 CASE WHEN fdi.export_revenue THEN 'yes' ELSE 'no' END AS export_revenue,
                 fdi.strategic_drivers,
@@ -811,7 +811,7 @@ class FDIMonthlyStaticViewPipeline(BaseViewPipeline):
              LEFT JOIN advisers_dataset acm ON inv.one_list_account_owner_id = acm.id
              LEFT JOIN teams_dataset acmt ON acm.team_id = acmt.id
              LEFT JOIN (
-                SELECT investment_project_id, max(interaction_date)::text as date_of_latest_interaction 
+                SELECT investment_project_id, max(interaction_date)::text as date_of_latest_interaction
                 FROM interactions_dataset i
                 WHERE investment_project_id IS NOT NULL
                 GROUP BY investment_project_id
@@ -827,27 +827,31 @@ class FDIMonthlyStaticViewPipeline(BaseViewPipeline):
             ORDER BY fdi.actual_land_date, fdi.estimated_land_date ASC
         )
         SELECT DISTINCT ON (unique_id)
-            actual_land_date, estimated_land_date, created_on, modified_on, date_of_latest_interaction, name, 
-            description,  project_reference,  total_investment, number_new_jobs, 
-            number_safeguarded_jobs, client_requirements, address_1, address_2, address_postcode, id, 
-            client_relationship_manager_id, client_relationship_manager_name, client_relationship_manager_team, 
-            account_manager_name, account_manager_team, fdi_type, investment_type, investor_company_id, 
-            investor_company_name, investor_company_comp_house_id, investor_company_headquarter_type, 
-            investor_company_company_tier, investor_company_sector, investor_company_address_1, investor_company_address_2, 
-            investor_company_uk_region, investor_company_country, investor_company_contact_name, 
-            investor_company_contact_marked_as_primary_contact, investor_company_contact_phone, 
-            investor_company_contact_email, investor_company_contact_accepts_dit_email_marketing,
-            specific_programme, stage, project_assurance_adviser_name, project_assurance_adviser_team, 
-            project_manager_name, project_manager_team, sector, uk_company_id, uk_company_name, 
-            uk_company_comp_house_id, uk_company_sector, uk_company_address_1, uk_company_address_2, uk_company_postcode,
-            uk_company_uk_region, uk_company_country, uk_company_contact_name, uk_company_contact_marked_as_primary_contact, 
-            uk_company_contact_phone,  uk_company_contact_email, uk_company_contact_accepts_dit_email_marketing, 
-            likelihood_to_land, fdi_value, created_by_name, created_by_team, modified_by_name, modified_by_team,
-            status, anonymous_description, associated_non_fdi_r_and_d_project_id, competing_countries,
-            team_members, investor_type, level_of_involvement, foreign_equity_investment, government_assistance, 
-            r_and_d_budget, non_fdi_r_and_d_budget, new_tech_to_uk, average_salary, referral_source_activity, 
-            referral_source_activity_website, referral_source_activity_marketing, delivery_partners, 
-            possible_uk_regions, actual_uk_regions, business_activities, project_arrived_in_triage_on, 
+            actual_land_date, estimated_land_date, created_on, modified_on,
+            date_of_latest_interaction, name, description,  project_reference,  total_investment,
+            number_new_jobs, number_safeguarded_jobs, client_requirements, address_1, address_2,
+            address_postcode, id, client_relationship_manager_id, client_relationship_manager_name,
+            client_relationship_manager_team, account_manager_name, account_manager_team, fdi_type,
+            investment_type, investor_company_id, investor_company_name,
+            investor_company_comp_house_id, investor_company_headquarter_type,
+            investor_company_company_tier, investor_company_sector, investor_company_address_1,
+            investor_company_address_2, investor_company_uk_region, investor_company_country,
+            investor_company_contact_name, investor_company_contact_marked_as_primary_contact,
+            investor_company_contact_phone, investor_company_contact_email,
+            investor_company_contact_accepts_dit_email_marketing, specific_programme, stage,
+            project_assurance_adviser_name, project_assurance_adviser_team, project_manager_name,
+            project_manager_team, sector, uk_company_id, uk_company_name, uk_company_comp_house_id,
+            uk_company_sector, uk_company_address_1, uk_company_address_2, uk_company_postcode,
+            uk_company_uk_region, uk_company_country, uk_company_contact_name,
+            uk_company_contact_marked_as_primary_contact, uk_company_contact_phone,
+            uk_company_contact_email, uk_company_contact_accepts_dit_email_marketing,
+            likelihood_to_land, fdi_value, created_by_name, created_by_team, modified_by_name,
+            modified_by_team, status, anonymous_description, associated_non_fdi_r_and_d_project_id,
+            competing_countries, team_members, investor_type, level_of_involvement,
+            foreign_equity_investment, government_assistance, r_and_d_budget,
+            non_fdi_r_and_d_budget, new_tech_to_uk, average_salary, referral_source_activity,
+            referral_source_activity_website, referral_source_activity_marketing, delivery_partners,
+            possible_uk_regions, actual_uk_regions, business_activities, project_arrived_in_triage_on,
             proposal_deadline, export_revenue, strategic_drivers, gross_value_added, gva_multiplier
         FROM fdi_report f    
     '''
