95d10c3bb1fcbc474e2271601e8556bd4b42e4d3
David Voong <voong.david@gmail.com>
2019-12-19 13:29:00 +0000

    fix table field names

--- dataflow/dags/dataset_pipelines.py
+++ dataflow/dags/dataset_pipelines.py
@@ -155,11 +155,11 @@ class CountriesOfInterestServiceCountriesOfInterest(BaseDatasetPipeline):
         '?orientation=records'.format(config.COUNTRIES_OF_INTEREST_BASE_URL)
     )
     field_mapping = [
-        ('company_id', 'character varying(100)'),
-        ('country_of_interest', 'character varying(2)'),
-        ('source', 'character varying(50)'),
-        ('source_id', 'character varying(100)'),
-        ('timestamp', 'timestamp with time zone'),
+        ('companyId', 'company_id', 'character varying(100)'),
+        ('countryOfInterest', 'country_of_interest', 'character varying(2)'),
+        ('source', 'source', 'character varying(50)'),
+        ('sourceId', 'source_id', 'character varying(100)'),
+        ('timestamp', 'timestamp', 'timestamp with time zone'),
     ]
 
 
