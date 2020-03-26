cca4655af4a40b2e18697a6e745c44b1f5fcaf3c
Kerin Cosford <kerin.cosford@methods.co.uk>
2020-03-26 13:45:29 +0000

    Update ConsentPipeline to use TableConfig

--- dataflow/dags/consent_pipelines.py
+++ dataflow/dags/consent_pipelines.py
@@ -7,12 +7,14 @@ from airflow.operators.python_operator import PythonOperator
 from dataflow import config
 from dataflow.dags import _PipelineDAG
 from dataflow.operators.common import fetch_from_hawk_api
+from dataflow.utils import TableConfig
 
 
 class _ConsentPipeline(_PipelineDAG):
     cascade_drop_tables = True
 
     source_url: str
+    table_config: TableConfig
 
     def get_fetch_operator(self) -> PythonOperator:
         return PythonOperator(
@@ -21,25 +23,33 @@ class _ConsentPipeline(_PipelineDAG):
                 fetch_from_hawk_api, hawk_credentials=config.CONSENT_HAWK_CREDENTIALS,
             ),
             provide_context=True,
-            op_args=[self.table_name, self.source_url],
+            op_args=[self.table_config.table_name, self.source_url],
         )
 
 
 class ConsentPipeline(_ConsentPipeline):
-    table_name = "consent_dataset"
+
     source_url = "{}/api/v1/person/datahub_export/?limit={}".format(
         config.CONSENT_BASE_URL, config.CONSENT_RESULTS_PER_PAGE
     )
-
-    field_mapping = [
-        ("id", sa.Column("id", sa.Integer, primary_key=True)),
-        ("key", sa.Column("key", sa.String)),
-        ("email", sa.Column("email", sa.String)),
-        ("phone", sa.Column("phone", sa.String)),
-        ("key_type", sa.Column("key_type", sa.String)),
-        ("created_at", sa.Column("created_at", sa.DateTime)),
-        ("modified_at", sa.Column("modified_at", sa.DateTime)),
-        ("current", sa.Column("current", sa.Boolean)),
-        ("email_marketing_consent", sa.Column("email_marketing_consent", sa.Boolean)),
-        ("phone_marketing_consent", sa.Column("phone_marketing_consent", sa.Boolean)),
-    ]
+    table_config = TableConfig(
+        table_name="consent_dataset",
+        field_mapping=[
+            ("id", sa.Column("id", sa.Integer, primary_key=True)),
+            ("key", sa.Column("key", sa.String)),
+            ("email", sa.Column("email", sa.String)),
+            ("phone", sa.Column("phone", sa.String)),
+            ("key_type", sa.Column("key_type", sa.String)),
+            ("created_at", sa.Column("created_at", sa.DateTime)),
+            ("modified_at", sa.Column("modified_at", sa.DateTime)),
+            ("current", sa.Column("current", sa.Boolean)),
+            (
+                "email_marketing_consent",
+                sa.Column("email_marketing_consent", sa.Boolean),
+            ),
+            (
+                "phone_marketing_consent",
+                sa.Column("phone_marketing_consent", sa.Boolean),
+            ),
+        ],
+    )
