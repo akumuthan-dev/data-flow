326eb6f91204e035fd50f7cf14f5597df071a819
Alexey Bezhan <al@fueledbylemons.com>
2020-03-31 13:01:19 +0100

    Use airflow DagBag to test pipelines are being imported correctly
    The previous test hasn't been updated after new pipelines were
    added, leaving some modules untested.
    
    This asserts the number of pipelines, so will need to be changed
    each time one is added or removed.

--- tests/test_dags.py
+++ tests/test_dags.py
@@ -1,51 +1,7 @@
-import pytest
-from airflow.models import DAG
-
-from dataflow.dags import (
-    activity_stream_pipelines,
-    canary,
-    company_matching,
-    consent_pipelines,
-    dataset_pipelines,
-    ons_pipelines,
-    spi_pipeline,
-)
-from dataflow.dags.csv_pipelines import (
-    csv_pipelines_daily,
-    csv_pipelines_monthly,
-    csv_pipelines_yearly,
-    csv_refresh_pipeline,
-)
+from airflow.models.dagbag import DagBag
 
 
-def test_canary_dag():
-    assert isinstance(canary.dag, DAG)
+def test_pipelines_dags():
+    dagbag = DagBag('dataflow')
 
-
-def test_canary_tweets():
-    assert (
-        canary.canary_tweet(task_name="test")
-        == "Canary task test was processed successfully"
-    )
-
-
-@pytest.mark.parametrize(
-    "module",
-    [
-        activity_stream_pipelines,
-        company_matching,
-        consent_pipelines,
-        csv_pipelines_daily,
-        csv_pipelines_monthly,
-        csv_pipelines_yearly,
-        csv_refresh_pipeline,
-        dataset_pipelines,
-        ons_pipelines,
-        spi_pipeline,
-    ],
-)
-def test_pipelines_dags(module):
-    dag_vars = [value for name, value in vars(module).items() if name.endswith("__dag")]
-
-    assert dag_vars
-    assert all(isinstance(dag, DAG) for dag in dag_vars)
+    assert dagbag.size() == 70
