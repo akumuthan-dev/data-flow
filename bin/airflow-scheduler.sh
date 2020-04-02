c7ec5bf3cd3ed3b777c38dc556a993e71e7b3138
Alexey Bezhan <al@fueledbylemons.com>
2020-04-02 13:47:50 +0100

    Reduce default pool slots to 16 by default
    With global task parallelism set to 32 this gives us 16 task slots
    and 16 sensor ones.
    
    We don't want to set tasks any higher for the moment since some of
    them are memory intensive and can cause an OOM.

--- bin/airflow-scheduler.sh
+++ bin/airflow-scheduler.sh
@@ -2,6 +2,7 @@
 
 airflow upgradedb
 
+airflow pool -s default_pool ${AIRFLOW_DEFAULT_POOL_SLOTS:-16} "Main tasks"
 airflow pool -s sensors ${AIRFLOW_SENSORS_POOL_SLOTS:-16} "External task sensors"
 
 airflow scheduler
