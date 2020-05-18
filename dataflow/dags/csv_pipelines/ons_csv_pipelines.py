e0e6bfc2169d06bf6072bd7951dc35530333d09c
Samuel Williams <samuelhwilliams@gmail.com>
2020-05-18 11:15:04 +0100

    Add included_in_eu28 via hard-coded list

--- dataflow/dags/csv_pipelines/ons_csv_pipelines.py
+++ dataflow/dags/csv_pipelines/ons_csv_pipelines.py
@@ -138,7 +138,20 @@ WITH rolling_import_totals AS (SELECT geography_code,
                                                  WHERE imports_t.direction = 'imports'
                                                    AND exports_t.direction = 'exports')
 SELECT
-    *
+    geography_code,
+    geography_name,
+    CASE
+        -- Taken from http://gss-data.org.uk/concept?uri=http%3A%2F%2Fgss-data.org.uk%2Fdef%2Fconcept%2Fons-partner-geography%2FB5
+        WHEN geography_name IN ('Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden') THEN 'yes'
+    ELSE 'no'
+    END AS included_in_eu28,
+    product_code,
+    period,
+    period_type,
+    measure,
+    value,
+    unit,
+    marker
 FROM
     imports_and_exports_with_rolling_totals
 WHERE
