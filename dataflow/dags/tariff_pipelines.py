4eb9c198144e44a6dddec560c0404a7a79874482
Nick Ross <nick.ir.ross@gmail.com>
2020-06-10 14:03:10 +0100

    Add new dataset for global uk tariff data

---
+++ dataflow/dags/tariff_pipelines.py
@@ -0,0 +1,39 @@
+from functools import partial
+
+import sqlalchemy as sa
+from airflow.operators.python_operator import PythonOperator
+
+from dataflow.dags import _PipelineDAG
+from dataflow.operators.common import fetch_from_api_endpoint
+from dataflow.utils import TableConfig
+
+
+class GlobalUKTariffPipeline(_PipelineDAG):
+    source_url = (
+        'https://www.check-future-uk-trade-tariffs.service.gov.uk/api/global-uk-tariff'
+    )
+    table_config = TableConfig(
+        schema='dit',
+        table_name='global_uk_tariff',
+        field_mapping=[
+            ('commodity', sa.Column('commodity', sa.String)),
+            ('description', sa.Column('description', sa.Text)),
+            ('cet_duty_rate', sa.Column('cet_duty_rate', sa.String)),
+            ('ukgt_duty_rate', sa.Column('ukgt_duty_rate', sa.String)),
+            ('change', sa.Column('change', sa.String)),
+            ('trade_remedy_applies', sa.Column('trade_remedy_applies', sa.Boolean)),
+            ('dumping_margin_applies', sa.Column('dumping_margin_applies', sa.Boolean)),
+            ('suspension_applies', sa.Column('suspension_applies', sa.Boolean)),
+            ('atq_applies', sa.Column('atq_applies', sa.Boolean)),
+        ],
+    )
+
+    def get_fetch_operator(self) -> PythonOperator:
+        return PythonOperator(
+            task_id='run-fetch',
+            python_callable=partial(
+                fetch_from_api_endpoint, results_key=None, next_key=None
+            ),
+            provide_context=True,
+            op_args=[self.table_config.table_name, self.source_url],
+        )
