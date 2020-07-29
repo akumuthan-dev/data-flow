72a120db6099e72004d7830fdb91a519cb10a52b
Charlotte Szostek <charlotte.szostek@digital.trade.gov.uk>
2020-07-29 10:46:39 +0100

    first try CH

--- dataflow/dags/company_matching.py
+++ dataflow/dags/company_matching.py
@@ -10,6 +10,7 @@ from dataflow.dags.dataset_pipelines import (
     CompaniesDatasetPipeline,
     ExportWinsWinsDatasetPipeline,
 )
+from dataflow.dags.companies_house_pipelines import CompaniesHouseCompaniesPipeline
 from dataflow.operators.company_matching import fetch_from_company_matching
 from dataflow.utils import TableConfig
 
@@ -83,3 +84,46 @@ class ExportWinsMatchingPipeline(_CompanyMatchingPipeline):
         FROM {ExportWinsWinsDatasetPipeline.table_config.table_name}
         ORDER BY id asc, created::timestamp desc
     """
+
+# # My addtions # TODO for non eu only? where is eu? pipeline?
+# class HMRCMatchingPipeline(_CompanyMatchingPipeline):
+#     # HMRCDatasetPipeline nothing existis. THere is a mapping seperately in hmrc_pipelines.py
+#     controller_pipeline = None # HMRCDatasetPipeline. onlu non eu imports and exports? where is EU data?
+#     dependencies = [controller_pipeline]
+#     company_match_query = f"""
+#         SELECT distinct on (id)
+#             id as id,
+#             company_name as company_name,
+#             customer_email_address as contact_email,
+#             NULLIF(cdms_reference, '') AS cdms_ref,
+#             null as postcode,
+#             null as copmanies_house_id,
+#             'dit.export-wins' as source,
+#             created::timestamp as datetime
+#         FROM {None.table_config.table_name}
+#         ORDER BY id asc, created::timestamp desc
+#     """
+
+class CompaniesHouseMatchingPipeline(_CompanyMatchingPipeline):
+    controller_pipeline = CompaniesHouseCompaniesPipeline # looks correct although no L1
+    dependencies = [CompaniesHouseCompaniesPipeline]
+    company_match_query = f"""
+        SELECT distinct on (id)
+            id as id,
+            company_name as company_name,
+            null as contact_email,
+            null as cdms_ref,
+            regaddress.pobox as postcode,
+            companies_house_id as copmanies_house_id,
+            'companies_house.companies.L1' as source,  
+            last_made_up_date as datetime
+        FROM {None.table_config.table_name}
+        ORDER BY id asc,  desc
+    """
+    # IS this fomat correct is L1 correct given CompaniesHouseCompaniesPipeline definition
+    # what is cdms_ref- why is it so important.
+    # IS companies_house id  SUPPOSED TO BE SPELT WRONG ??
+    # Why do we care about record timestamp for matching? Need to check usage to see what else may be useful
+    # Going with accoun last made up for now but seems weird to === apples and oranges.
+    # id order and timestamp order - so seems most recent!
+    # last_made_up_date vs return_last_made_up_date. TODO check this
