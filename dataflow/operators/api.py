5ad8ca405cf220f2fbbe18c5ed3fe446f66a86f3
Michal Charemza <michal@charemza.name>
2020-10-20 16:31:21 +0100

    feat: lower log level and avoid Sentry on Activity Stream failure (actually this time)
    We seem to get a separate Sentry event for every Hawk failure, even if
    they're all raised in the same place. This avoids any Sentry event on
    all but the last failure.
    
    This was initially attempted in
    890ff970845c97bf6de356a6dda3d790bbf6af58, but that changed the wrong
    _hawk_api_request function.
    
    Leaving potentially merging the two functions until later.

--- dataflow/operators/api.py
+++ dataflow/operators/api.py
@@ -32,7 +32,7 @@ def _hawk_api_request(
     try:
         response.raise_for_status()
     except requests.exceptions.HTTPError:
-        logger.error(f"Request failed: {response.text}")
+        logger.warning(f"Request failed: {response.text}")
         raise
 
     response_json = response.json()
