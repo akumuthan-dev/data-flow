bcdb1f34e163d93d49498e691c96c69f7b635bdf
Mohammad Abbas <mohammad.abbas@digital.trade.gov.uk>
2020-11-25 12:52:34 +0000

    feature: use comtrade bulk api for fetching goods and services

--- dataflow/dags/comtrade_pipelines.py
+++ dataflow/dags/comtrade_pipelines.py
@@ -13,49 +13,6 @@ from dataflow.utils import TableConfig
 
 class ComtradeGoodsPipeline(_PipelineDAG):
     allow_null_columns = True
-    table_config = TableConfig(
-        schema="un",
-        table_name="comtrade__goods",
-        field_mapping=[
-            (None, sa.Column("id", sa.Integer, primary_key=True, autoincrement=True)),
-            ('Classification', sa.Column('classification', sa.String, nullable=False, index=True)),
-            ('Year', sa.Column('year', sa.SmallInteger, nullable=False, index=True)),
-            ('Aggregate Level', sa.Column('aggregate_level', sa.BigInteger, nullable=False, index=True)),
-            ('Is Leaf Code', sa.Column('is_leaf_code', sa.Boolean)),
-            ('Trade Flow Code', sa.Column('trade_flow_code', sa.BigInteger, nullable=False)),
-            ('Trade Flow', sa.Column('trade_flow', sa.String, nullable=False)),
-            ('Reporter Code', sa.Column('reporter_code', sa.BigInteger, nullable=False, index=True)),
-            ('Reporter', sa.Column('reporter', sa.String, nullable=False)),
-            ('Reporter ISO', sa.Column('reporter_iso', sa.String, nullable=False)),
-            ('Partner Code', sa.Column('partner_code', sa.BigInteger, nullable=False, index=True)),
-            ('Partner', sa.Column('partner', sa.String, nullable=False)),
-            ('Partner ISO', sa.Column('partner_iso', sa.String)),
-            ('2nd Partner Code', sa.Column('second_partner_code', sa.BigInteger)),
-            ('2nd Partner', sa.Column('second_partner', sa.String)),
-            ('2nd Partner ISO', sa.Column('second_partner_iso', sa.String)),
-            ('Customs Proc. Code', sa.Column('customs_proc_code', sa.String)),
-            ('Customs', sa.Column('customs', sa.String)),
-            (
-                'Mode of Transport Code',
-                sa.Column('mode_of_transport_code', sa.BigInteger),
-            ),
-            ('Mode of Transport', sa.Column('mode_of_transport', sa.String)),
-            ('Commodity Code', sa.Column('commodity_code', sa.String, nullable=False)),
-            ('Commodity', sa.Column('commodity', sa.String, nullable=False)),
-            ('Qty Unit Code', sa.Column('quantity_unit_code', sa.BigInteger, nullable=False)),
-            ('Alt Qty Unit Code', sa.Column('alt_quantity_unit_code', sa.String)),
-            ('Gross weight (kg)', sa.Column('gross_weight_kg', sa.Numeric)),
-            ('Trade Value (US$)', sa.Column('trade_value_usd', sa.Numeric)),
-            ('CIF Trade Value (US$)', sa.Column('cif_trade_value_usd', sa.Numeric)),
-            ('FOB Trade Value (US$)', sa.Column('fob_trade_value_usd', sa.Numeric)),
-        ],
-        transforms=[
-            lambda record, table_config, contexts: {
-                **record,
-                'Is Leaf Code': bool(int(record['Is Leaf Code'])),
-            }
-        ],
-    )
 
     def get_fetch_operator(self) -> PythonOperator:
         return PythonOperator(
@@ -65,11 +22,59 @@ class ComtradeGoodsPipeline(_PipelineDAG):
             op_args=[self.table_config.table_name],  # pylint: disable=no-member
         )
 
+    table_config = TableConfig(
+        schema="un",
+        table_name="comtrade__goods",
+        field_mapping=[
+            (None, sa.Column("id", sa.Integer, primary_key=True, autoincrement=True)),
+            (
+                'Classification',
+                sa.Column('classification', sa.String, nullable=False, index=True),
+            ),
+            ('Period', sa.Column('period', sa.SmallInteger, nullable=False, index=True)),
+            ('Period Desc.', sa.Column('period_desc', sa.SmallInteger, nullable=False, index=True)),
+            ('Year', sa.Column('year', sa.SmallInteger, nullable=False, index=True)),
+            (
+                'Aggregate Level',
+                sa.Column('aggregate_level', sa.BigInteger, nullable=False, index=True),
+            ),
+            ('Is Leaf Code', sa.Column('is_leaf_code', sa.Boolean)),
+            (
+                'Trade Flow Code',
+                sa.Column('trade_flow_code', sa.BigInteger, nullable=False),
+            ),
+            ('Trade Flow', sa.Column('trade_flow', sa.String, nullable=False)),
+            (
+                'Reporter Code',
+                sa.Column('reporter_code', sa.BigInteger, nullable=False, index=True),
+            ),
+            ('Reporter', sa.Column('reporter', sa.String, nullable=False)),
+            ('Reporter ISO', sa.Column('reporter_iso', sa.String)),
+            (
+                'Partner Code',
+                sa.Column('partner_code', sa.BigInteger, nullable=False, index=True),
+            ),
+            ('Partner', sa.Column('partner', sa.String, nullable=False)),
+            ('Partner ISO', sa.Column('partner_iso', sa.String)),
+            ('Commodity Code', sa.Column('commodity_code', sa.String, nullable=False)),
+            ('Commodity', sa.Column('commodity', sa.String, nullable=False)),
+            (
+                'Qty Unit Code',
+                sa.Column('quantity_unit_code', sa.BigInteger, nullable=False),
+            ),
+            ('Trade Value (US$)', sa.Column('trade_value_usd', sa.Numeric)),
+        ],
+        transforms=[
+            lambda record, table_config, contexts: {
+                **record,
+                'Is Leaf Code': bool(int(record['Is Leaf Code'])),
+            }
+        ],
+    )
+
 
 class ComtradeServicesPipeline(_PipelineDAG):
-    table_config = TableConfig(
-        schema="un", table_name="comtrade__services", field_mapping=[],
-    )
+    allow_null_columns = True
 
     def get_fetch_operator(self) -> PythonOperator:
         return PythonOperator(
@@ -78,3 +83,49 @@ class ComtradeServicesPipeline(_PipelineDAG):
             provide_context=True,
             op_args=[self.table_config.table_name],  # pylint: disable=no-member
         )
+
+    table_config = TableConfig(
+        schema="un",
+        table_name="comtrade__services",
+        field_mapping=[
+            (None, sa.Column("id", sa.Integer, primary_key=True, autoincrement=True)),
+            (
+                'Classification',
+                sa.Column('classification', sa.String, nullable=False, index=True),
+            ),
+            ('Year', sa.Column('year', sa.SmallInteger, nullable=False, index=True)),
+            ('Period', sa.Column('period', sa.SmallInteger, nullable=False, index=True)),
+            ('Period Desc.', sa.Column('period_desc', sa.SmallInteger, nullable=False, index=True)),
+            (
+                'Aggregate Level',
+                sa.Column('aggregate_level', sa.BigInteger, nullable=False, index=True),
+            ),
+            ('Is Leaf Code', sa.Column('is_leaf_code', sa.Boolean)),
+            (
+                'Trade Flow Code',
+                sa.Column('trade_flow_code', sa.BigInteger, nullable=False),
+            ),
+            ('Trade Flow', sa.Column('trade_flow', sa.String, nullable=False)),
+            (
+                'Reporter Code',
+                sa.Column('reporter_code', sa.BigInteger, nullable=False, index=True),
+            ),
+            ('Reporter', sa.Column('reporter', sa.String, nullable=False)),
+            ('Reporter ISO', sa.Column('reporter_iso', sa.String)),
+            (
+                'Partner Code',
+                sa.Column('partner_code', sa.BigInteger, nullable=False, index=True),
+            ),
+            ('Partner', sa.Column('partner', sa.String, nullable=False)),
+            ('Partner ISO', sa.Column('partner_iso', sa.String)),
+            ('Commodity Code', sa.Column('commodity_code', sa.String, nullable=False)),
+            ('Commodity', sa.Column('commodity', sa.String, nullable=False)),
+            ('Trade Value (US$)', sa.Column('trade_value_usd', sa.Numeric)),
+        ],
+        transforms=[
+            lambda record, table_config, contexts: {
+                **record,
+                'Is Leaf Code': bool(int(record['Is Leaf Code'])),
+            }
+        ],
+    )
--- dataflow/operators/comtrade.py
+++ dataflow/operators/comtrade.py
@@ -1,9 +1,11 @@
 import codecs
 import csv
+import io
 import itertools
 import time
 import json
 import logging
+import zipfile
 
 import backoff
 import requests
@@ -31,26 +33,13 @@ def fetch_comtrade_goods_data(
         'Partner Code',
         'Partner',
         'Partner ISO',
-        '2nd Partner Code',
-        '2nd Partner',
-        '2nd Partner ISO',
-        'Customs Proc. Code',
-        'Customs',
-        'Mode of Transport Code',
-        'Mode of Transport',
         'Commodity Code',
         'Commodity',
         'Qty Unit Code',
         'Qty Unit',
         'Qty',
-        'Alt Qty Unit Code',
-        'Alt Qty Unit',
-        'Alt Qty',
         'Netweight (kg)',
-        'Gross weight (kg)',
         'Trade Value (US$)',
-        'CIF Trade Value (US$)',
-        'FOB Trade Value (US$)',
         'Flag',
     ]
     _fetch(s3, 'C', expected_keys)
@@ -75,15 +64,9 @@ def fetch_comtrade_services_data(
         'Partner Code',
         'Partner',
         'Partner ISO',
-        'Mode of Transport Code',
-        'Mode of Transport',
         'Commodity Code',
         'Commodity',
-        'Netweight (kg)',
-        'Gross weight (kg)',
         'Trade Value (US$)',
-        'CIF Trade Value (US$)',
-        'FOB Trade Value (US$)',
         'Flag',
     ]
     _fetch(s3, 'S', expected_keys)
@@ -95,21 +78,10 @@ def _fetch(s3, trade_type, expected_keys):
     if years_objs['more']:
         raise Exception('Unable to fetch all years')
 
-    with _download('https://comtrade.un.org/Data/cache/reporterAreas.json') as response:
-        reporter_areas_objs = json.loads(response.content)
-    if reporter_areas_objs['more']:
-        raise Exception('Unable to fetch all reporter areas')
-
     years = [
         year_obj['id']
         for year_obj in years_objs['results']
         if year_obj['id'].lower() != 'all'
-    ]
-
-    reporter_areas = [
-        reporter_area['id']
-        for reporter_area in reporter_areas_objs['results']
-        if reporter_area['id'].lower() != 'all'
     ]
 
     def paginate(items, num_per_page):
@@ -122,39 +94,33 @@ def _fetch(s3, trade_type, expected_keys):
         if page:
             yield page
 
-    def download(trade_type, expected_keys, period_and_partner_area_pages):
-        frequency_annual = 'A'
-        classification_hs = 'HS'
-        partner_areas_all = 'all'
-        trade_regime_all = 'all'
-        desired_commodity_codes_total = 'ALL'
+    def file_from_zip(zip_bytes):
+        with zipfile.ZipFile(io.BytesIO(zip_bytes)) as archive:
+            name = archive.namelist()[0]
+            logger.info('Opening file in zip %s', name)
+            with archive.open(name, "r") as file:
+                yield file
 
-        for period_page, reporter_page in period_and_partner_area_pages:
-            for row in _download_dicts(
-                'https://comtrade.un.org/api/get',
-                (
-                    ('max', '100000'),
-                    ('fmt', 'csv'),
-                    ('type', trade_type),
-                    ('freq', frequency_annual),
-                    ('px', classification_hs),
-                    ('ps', ','.join(period_page)),
-                    ('r', ','.join(reporter_page)),
-                    ('p', partner_areas_all),
-                    ('rg', trade_regime_all),
-                    ('cc', desired_commodity_codes_total),
-                    ('token', config.COMTRADE_TOKEN),
-                ),
-            ):
+    def get_files(trade_type, expected_keys, periods):
+        frequency = 'A'
+        classification = 'HS' if trade_type == 'C' else 'EB02'
+        for period in periods:
+            yield from file_from_zip(
+                _download(
+                    f'https://comtrade.un.org/api/get/bulk/{trade_type}/{frequency}/{period}/all/{classification}',
+                    params=(('token', config.COMTRADE_TOKEN),),
+                ).content
+            )
+
+    def get_dicts(files):
+        for f in files:
+            for row in csv.DictReader(codecs.iterdecode(f, 'utf-8-sig')):
                 if list(row.keys()) != expected_keys:
                     raise Exception('Unexpected columns {}'.format(row.keys()))
-
                 yield {k: v if v else None for k, v in row.items()}
 
-    period_pages = paginate(years, 5)
-    partner_area_pages = paginate(reporter_areas, 5)
-    period_and_partner_area_pages = itertools.product(period_pages, partner_area_pages)
-    result_records = download(trade_type, expected_keys, period_and_partner_area_pages)
+    files = get_files(trade_type, expected_keys, years)
+    result_records = get_dicts(files)
     results_pages = paginate(result_records, 10000)
 
     for i, page in enumerate(results_pages):
@@ -174,30 +140,3 @@ def _download(source_url, params=()):
     response = requests.get(source_url, stream=True, params=params)
     response.raise_for_status()
     return response
-
-
-def throttled_generator(generator_func):
-    ''' Forces an interval of 1 second between the completion of every call to the generator '''
-
-    seconds_between_calls = 1.0
-    previous = float('-infinity')
-
-    def _throttled_func(*args, **kwargs):
-        nonlocal previous
-
-        now = time.monotonic()
-        to_sleep = max(0, seconds_between_calls - (now - previous))
-        logger.info('Sleeping for %s seconds', to_sleep)
-        time.sleep(to_sleep)
-
-        yield from generator_func(*args, **kwargs)
-
-        previous = time.monotonic()
-
-    return _throttled_func
-
-
-@throttled_generator
-def _download_dicts(source_url, params=()):
-    with _download(source_url, params=params) as response:
-        yield from csv.DictReader(codecs.iterdecode(response.iter_lines(), 'utf-8'))
