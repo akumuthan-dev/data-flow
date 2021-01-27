fc94da45b1db00fc27e62c59d592156ca472a582
akumuthan-dev <58772212+akumuthan-dev@users.noreply.github.com>
2021-01-27 16:20:19 +0000

    Update bigquery_pipelines.py
    Commented out possible field mappings.

--- dataflow/dags/bigquery_pipelines.py
+++ dataflow/dags/bigquery_pipelines.py
@@ -5,6 +5,7 @@ from dataflow.dags import _PipelineDAG
 from dataflow.operators.bigquery import fetch_from_bigquery
 from dataflow.utils import TableConfig
 
+from datetime import date,timedelta
 
 class GreatBetaGoogleAnalyticsPipeline(_PipelineDAG):
     schedule_interval = '0 0 10 * *'
@@ -16,6 +17,29 @@ class GreatBetaGoogleAnalyticsPipeline(_PipelineDAG):
             ('id', sa.Column('id', sa.String(8), primary_key=True)),
             ('SomeName', sa.Column('some_name', sa.String)),
         ],
+        '''
+        field_mapping=[
+            ('clientId', sa.Column('clientId', sa.String)),
+            ('fullVisitorId', sa.Column('fullVisitorId', sa.String)),
+            ('userId', sa.Column('userId', sa.String)),
+            ('visitNumber', sa.Column('visitNumber', sa.Numeric)),
+            ('visitId', sa.Column('visitId', sa.Numeric)),
+            ('visitStartTime', sa.Column('visitStartTime', sa.Numeric)),
+            ('date', sa.Column('date', sa.String)),
+            ('bounces', sa.Column('bounces', sa.Numeric)),
+            ('hits', sa.Column('hits', sa.Numeric)),
+            ('newVisits', sa.Column('newVisits', sa.Numeric)),
+            ('pageviews', sa.Column('pageviews', sa.Numeric)),
+            ('visits', sa.Column('visits', sa.Numeric)),
+            ('browser', sa.Column('browser', sa.String)),
+            ('browserVersion', sa.Column('browserVersion', sa.String)),
+            ('deviceCategory', sa.Column('deviceCategory', sa.String)),
+            ('mobileDeviceInfo', sa.Column('mobileDeviceInfo', sa.String)),
+            ('mobileDeviceModel', sa.Column('mobileDeviceModel', sa.String)),
+            ('operatingSystem', sa.Column('operatingSystem', sa.String)),
+            ('operatingSystemVersion', sa.Column('operatingSystemVersion'))
+        ],
+        '''
     )
 
     def get_fetch_operator(self) -> PythonOperator:
@@ -26,3 +50,4 @@ class GreatBetaGoogleAnalyticsPipeline(_PipelineDAG):
             op_args=[self.table_config.table_name],  # pylint: disable=no-member
             retries=self.fetch_retries,
         )
+
